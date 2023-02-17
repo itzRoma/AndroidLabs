1d4622fb00ab45977afe45320cae55386f41b0f8
itzRoma <romabondarenko03@gmail.com>
2023-02-17 02:24:15 +0200

    lab1: upload

---
+++ app/lab1/src/main/java/com/itzroma/kpi/semester6/androidlabs/lab1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.itzroma.kpi.semester6.androidlabs.lab1;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText passwordInputET;
+
+    private RadioGroup visibilityOptionsRG;
+
+    private Button okButton;
+
+    private TextView retrievedPassTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        passwordInputET = findViewById(R.id.password_input);
+        visibilityOptionsRG = findViewById(R.id.visibility_options);
+        okButton = findViewById(R.id.ok_button);
+        retrievedPassTV = findViewById(R.id.retrieved_pass);
+
+        visibilityOptionsRG.setOnCheckedChangeListener((radioGroup, i) -> {
+            int cursorStart = passwordInputET.getSelectionStart();
+            int cursorEnd = passwordInputET.getSelectionEnd();
+
+            if (i == findViewById(R.id.hide_rb).getId()) {
+                passwordInputET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            } else if (i == findViewById(R.id.show_rb).getId()) {
+                passwordInputET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+            }
+
+            passwordInputET.setSelection(cursorStart, cursorEnd);
+        });
+
+        okButton.setOnClickListener(v -> retrievedPassTV.setText(passwordInputET.getText()));
+    }
+}
